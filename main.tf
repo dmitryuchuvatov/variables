5026017c1a8448aceb673d8ff31328937b8a70e7
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2023-05-08 13:35:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_vpc" "example" {
-  cidr_block = "172.0.0.0/16"
-  tags = {
-    Name = "example_vpc"
-  }
+output "myvar" {
+  value = var.myvar
 }
